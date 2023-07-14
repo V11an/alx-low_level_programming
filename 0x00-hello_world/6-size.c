120cc83a19dd12449957d71054d7888df12505e6
V11an <allankertich@gmail.com>
2023-07-14 01:35:04 +0000

    Program that prints various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,12 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float e;
+	float f;
 
 	printf("Size of char: %lu bytes(s)\n", (unsigned long)sizeof(a));
 	printf("Size of int: %lu bytes(s)\n", (unsigned long)sizeof(b));
 	printf("Size of long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 	printf("Size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(e));
+	printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
