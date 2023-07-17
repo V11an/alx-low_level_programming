cfced684d7c9287cd27ef2d79f94b95f67cf9363
V11an <allankertich@gmail.com>
2023-07-17 14:53:36 -0700

    changes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,19 @@ int main(void)
 	int first_num, second_num;
 
 	first_num = 0;
-	while(first_num < 100)
+	while (first_num < 100)
 	{
 		 second_num = 0;
-		 while(second_num < 100)
+		 while (second_num < 100)
 		 {
-			 if(first_num < second_num)
+			 if (first_num < second_num)
 			 {
 				 putchar((first_num / 10) + 48);
 				 putchar((first_num % 10) + 48);
 				 putchar(' ');
 				 putchar((second_num / 10) + 48);
 				 putchar((second_num % 10) + 48);
-				 if(first_num != 98 || second_num != 99)
+				 if (first_num != 98 || second_num != 99)
 				 {
 					 putchar(',');
 					 putchar(' ');
