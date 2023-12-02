f1608c791f5ba6853451899d43f96dde7f9b0dea
V11an <allankertich@gmail.com>
2023-12-02 04:20:51 +0300

    ['Betty'] = 'Cool'

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	return (0);
+}
+
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+	return (0);
+}
+
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	return (0);
+}
+
+void shash_table_print(const shash_table_t *ht)
+{
+	return (0);
+}
+
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	return (0);
+}
+
+void shash_table_delete(shash_table_t *ht)
+{
+	return (0);
+}
